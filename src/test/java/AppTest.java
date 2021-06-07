455d8bbe43c948c1b3c4f5cc32f7edea47a0d1d8
Lalit Sukhwal <34391440+lalit29@users.noreply.github.com>
2021-06-07 10:25:24 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
